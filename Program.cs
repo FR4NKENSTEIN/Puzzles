70d8cf36f30f077bcbbee711f11a5fe3a9e78d19
FR4NKENSTEIN <jacobleesmithy@gmail.com>
2019-10-02 15:27:47 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Puzzling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
